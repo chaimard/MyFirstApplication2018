97fe177c0c4c8ce62a22146cc6c4c8e74f9bed67
ckm <chaiamrd@email.krirk.ac.th>
2019-01-09 15:55:38 +0700

    add shop name edit jan2019

--- app/src/main/java/com/example/ckm/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/example/ckm/myfirstapplication/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.ckm.myfirstapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+//        button
+        final Button btn1 = (Button) findViewById(R.id.button1);
+
+//        textview
+        final TextView textV2 = (TextView) findViewById(R.id.textView2);
+
+//        input text
+        final TextView editT1 = (TextView) findViewById(R.id.editText1);
+
+//        perform action on click
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String strTxt = editT1.getText().toString();
+                textV2.setText("Helllo Khun: " + strTxt);
+            }
+        });
+
+    }// void
+
+  /*  @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }*/
+
+
+}// main
